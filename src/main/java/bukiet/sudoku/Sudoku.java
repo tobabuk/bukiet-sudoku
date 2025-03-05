258532f158eb583e6afc161c55252b48d616a31f
Toba Bukiet <tobabukiet@gmail.com>
2025-03-04 19:32:47 -0500

    asserts all errors, broke up the getError method

--- src/main/java/bukiet/sudoku/Sudoku.java
+++ src/main/java/bukiet/sudoku/Sudoku.java
@@ -34,8 +34,16 @@ public class Sudoku {
 
     public List<String> getErrors() {
         List<String> stringList = new ArrayList<>();
+        stringList.addAll(checkRow());
+        stringList.addAll(checkCol());
+        stringList.addAll(checkBox());
+        return stringList;
+    }
 
-        //check that the row is correct
+    //check that the row is correct
+
+    public List<String> checkRow() {
+        List<String> stringList = new ArrayList<>();
         for (int row = 0; row < 9; row++) {
             boolean[] checked = new boolean[10];
             for (int col = 0; col < 9; col++) {
@@ -48,9 +56,15 @@ public class Sudoku {
                     checked[num] = true;
                 }
             }
-
         }
-        // check that the collum is correct
+        return stringList;
+
+    }
+
+
+    public List<String> checkCol() {
+        List<String> stringList = new ArrayList<>();
+        // check that the colum is correct
         for (int col = 0; col < 9; col++) {
             boolean[] checked = new boolean[10];
             for (int row = 0; row < 9; row++) {
@@ -58,14 +72,18 @@ public class Sudoku {
                 if (num < 1 || num > 9) {
                     stringList.add(String.format("%d is not a valid number", num));
                 } else if (checked[num]) {
-                    stringList.add(String.format("There is a duplicate number %d in collum %d", num, col));
+                    stringList.add(String.format("There is a duplicate number %d in column %d", num, col));
                 } else {
                     checked[num] = true;
                 }
             }
-
         }
+        return stringList;
+    }
 
+
+    public List<String> checkBox() {
+        List<String> stringList = new ArrayList<>();
         for (int row = 0; row < 9; row += 3) {
 
             for (int col = 0; col < 9; col += 3) {
@@ -77,7 +95,7 @@ public class Sudoku {
                         if (num < 1 || num > 9) {
                             stringList.add(String.format("%d is not a valid number", num));
                         } else if (checked[num]) {
-                            stringList.add(String.format("There is a duplicate number %d in box %d", num, board[i][j]));
+                            stringList.add(String.format("There is a duplicate number %d at row %d column %d", num, row, col));
                         } else {
                             checked[num] = true;
                         }
@@ -90,8 +108,8 @@ public class Sudoku {
         return stringList;
     }
 
-
 }
+
 
 
 
--- src/test/java/bukiet/sudoku/SudokuTest.java
+++ src/test/java/bukiet/sudoku/SudokuTest.java
@@ -29,20 +29,30 @@ class SudokuTest {
 
     @Test
     public void incorrectSudoku() {
-        int[][] trueBoard = {{7, 9, 2, 1, 5, 4, 3, 8, 6},
-                {6, 4, 3, 8, 2, 7, 1, 5, 9},
+        int[][] falseBoard = {{7, 9, 2, 1, 5, 4, 3, 8, 6},
+                {6, 4, 3, 8, 30, 7, 1, 5, 9},
                 {8, 5, 1, 3, 9, 7, 7, 2, 4},
                 {2, 6, 5, 9, 7, 3, 8, 4, 1},
                 {4, 8, 9, 5, 6, 1, 2, 7, 3},
                 {3, 1, 7, 4, 8, 2, 9, 6, 5},
-                {1, 3, 6, 7, 4, 8, 5, 9, 2},
-                {9, 7, 4, 2, 1, 5, 6, 3, 8},
+                {3, 3, 6, 7, 4, 8, 5, 9, 2},
+                {9, 7, 4, 2, 1, 5, 2, 3, 8},
                 {5, 2, 8, 6, 3, 9, 4, 1, 7}};
 
-        Sudoku su = new Sudoku(trueBoard);
+        Sudoku su = new Sudoku(falseBoard);
         List<String> results = su.getErrors();
-        assertFalse(results.isEmpty());
+        System.out.println(results);
 
+        assertFalse(results.isEmpty());
+        assertTrue(results.contains("There is a duplicate number 7 in row 2"),
+                "Expected duplicate error for 7 in row 2");
+
+        assertTrue(results.contains("There is a duplicate number 3 in column 0"),
+                "Expected duplicate error for 3 in column 0");
+        assertTrue(results.contains("There is a duplicate number 2 at row 6 column 6"),
+                "Expected duplicate error for 2 at row 6 column 6");
+
+        assertTrue(results.contains("30 is not a valid number"), "Expected a valid number");
 
     }
 
